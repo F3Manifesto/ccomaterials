4fb79677bfa2e41f4da9ed4e23ca1393d713bf80
Emma-Jane MacKinnon-Lee <72898465+emmajane1313@users.noreply.github.com>
2022-02-07 23:39:30 -0500

    add

--- icoinedweb3fashion/3dfiles/index.vue
+++ icoinedweb3fashion/3dfiles/index.vue
@@ -1,10 +1,12 @@
-// Files were too big to directly upload to the repo, see both Pinata hashes & corresponding Google drive links for download
+// Files were too big to directly upload to the repo, see both Pinata hashes and corresponding Google drive links for download
 
-CLO3D File Drive Link: https://drive.google.com/file/d/13OHNt9abFrbEGD9RkO-QnJhyfc120_p2/view?usp=sharing
-CLO3D File Pinata Link: https://digitalax.mypinata.cloud/ipfs/QmQE5gawvqXUpjrJTb8qZANBHDAu3WopN785hD8V4AmLrF
+{
+  "CLO3D File Drive Link": https://drive.google.com/file/d/13OHNt9abFrbEGD9RkO-QnJhyfc120_p2/view?usp=sharing,
+  "CLO3D File Pinata Link": https://digitalax.mypinata.cloud/ipfs/QmQE5gawvqXUpjrJTb8qZANBHDAu3WopN785hD8V4AmLrF,
 
-Blender File Drive Link: https://drive.google.com/file/d/1d83aZC9loUtW5k5P8hK_P1bEy7BTEeqx/view?usp=sharing
-Blender File Pinata Link: https://digitalax.mypinata.cloud/ipfs/QmQD82CqW9KrzXb9DeY3rTX2ew9yTZTMiDdC1YSdJmTBsa
+  "Blender File Drive Link": https://drive.google.com/file/d/1d83aZC9loUtW5k5P8hK_P1bEy7BTEeqx/view?usp=sharing,
+  "Blender File Pinata Link": https://digitalax.mypinata.cloud/ipfs/QmQD82CqW9KrzXb9DeY3rTX2ew9yTZTMiDdC1YSdJmTBsa,
 
-Blender 1 File Drive Link: https://drive.google.com/file/d/1Z1_j-Fwqsg8X3DrKB69eiWxEnMcTAQvI/view?usp=sharing
-Blender 1 File Pinata Link: https://digitalax.mypinata.cloud/ipfs/QmYhwC6KrzQhDYF791jkXnvmFC2DisFMTxE53xBtH1msCr
+  "Blender 1 File Drive Link": https://drive.google.com/file/d/1Z1_j-Fwqsg8X3DrKB69eiWxEnMcTAQvI/view?usp=sharing,
+  "Blender 1 File Pinata Link": https://digitalax.mypinata.cloud/ipfs/QmYhwC6KrzQhDYF791jkXnvmFC2DisFMTxE53xBtH1msCr
+}
