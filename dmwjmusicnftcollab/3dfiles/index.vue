0243b8edbd06b8d686fe11f24367ed81701faedc
Emma-Jane MacKinnon-Lee <72898465+emmajane1313@users.noreply.github.com>
2022-02-12 00:41:08 -0500

    add files

---
+++ dmwjmusicnftcollab/3dfiles/index.vue
@@ -0,0 +1,10 @@
+// Files were too big to directly upload to the repo, see both Pinata hashes and corresponding Google drive links for download
+
+{
+  "CLO3D File Drive Link": https://drive.google.com/file/d/1riWJSTlGNfV_cYAoDX_pRyI4Ltqq2W_B/view?usp=sharing,
+  "CLO3D File Pinata Link": https://digitalax.mypinata.cloud/ipfs/QmajS6PWTBm5doAtbyWsSkc9EVhjfCZ5SqhA9MMG7LocQL,
+
+  "Blender File Drive Link": https://drive.google.com/file/d/14dgoldQO2bf3zqBiy9u3zUKieDep4CN-/view?usp=sharing,
+  "Blender File Pinata Link": https://digitalax.mypinata.cloud/ipfs/QmRuPmfUpjNKggUaMrfTcT5TMDhhvbLpF9zz2ep6Tuf5dh,
+
+}
