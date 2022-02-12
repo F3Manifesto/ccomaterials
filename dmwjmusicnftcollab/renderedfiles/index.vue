70ee4d6435f14772968cb7790092683334161983
Emma-Jane MacKinnon-Lee <72898465+emmajane1313@users.noreply.github.com>
2022-02-12 01:53:22 -0500

    Update index.vue

--- dmwjmusicnftcollab/renderedfiles/index.vue
+++ dmwjmusicnftcollab/renderedfiles/index.vue
@@ -1,1 +1,6 @@
+//
+"Pinata Upload Blender Render": https://digitalax.mypinata.cloud/ipfs/QmamLsVH13jEmHRkMf2gyNDW9UJ5K4HvjfhzsRd2oh58gj,
+"Drive Upload Blender Render": https://drive.google.com/file/d/1RZlKec1s_Do0ZNm23Ish1RtIRi6oMz1x/view?usp=sharing,
 
+"Pinata Upload Final Render w/ Audio": https://digitalax.mypinata.cloud/ipfs/QmamLsVH13jEmHRkMf2gyNDW9UJ5K4HvjfhzsRd2oh58gj,
+"Drive Upload Final Render w/ Audio": https://drive.google.com/file/d/14luqZbGiig521EMpGukm9iGY59oEk_YH/view?usp=sharing
