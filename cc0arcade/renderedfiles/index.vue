ca8b585a2b0d22652d507ec09f5b2e9c9283e7f8
Emma-Jane MacKinnon-Lee <72898465+emmajane1313@users.noreply.github.com>
2022-03-12 20:54:16 -0500

    add

--- cc0arcade/renderedfiles/index.vue
+++ cc0arcade/renderedfiles/index.vue
@@ -1,4 +1,12 @@
 //
 "Pinata Upload Blender Render": https://digitalax.mypinata.cloud/ipfs/QmXN3wyn8ZBucq4TG4yx7c95DMax4BKCuxYHpKNKoavszi,
 
-"Pinata Upload Final Render": https://digitalax.mypinata.cloud/ipfs/,
+"Pinata Upload First Video Render": https://digitalax.mypinata.cloud/ipfs/QmVAJEbs7j76oN2snPGpoMG1F2xKp94sNA9Y436SHR2o6T,
+
+"Pinata Upload Feedback Video Render": https://digitalax.mypinata.cloud/ipfs/QmZcW34p4dXzuXQcsN2agdvpjxVZDJ5Q1dx8SBvM9NL1Aj,
+
+"Pinata Upload Glitch Video Render": https://digitalax.mypinata.cloud/ipfs/QmZirJjELQM9T2XnyRLrxg5cTAftNkV6KiDXKM2axCZ1WG,
+
+"Pinata Upload Final Render without Audio": https://digitalax.mypinata.cloud/ipfs/QmeBMRVPBxifsTZWncmzV27ES9zvQQUVna1Tm8vmW7qcoW,
+
+"Pinata Upload Final Render with Audio": https://digitalax.mypinata.cloud/ipfs/QmcQ71QYEZf6w7naXsRQqLPNqifFWURkXnP9aUFm7soxni
