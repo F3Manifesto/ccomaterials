732ecb508f25cd1c09cb3a72f41edc047611d721
Emma-Jane MacKinnon-Lee <72898465+emmajane1313@users.noreply.github.com>
2022-03-20 05:52:41 -0400

    add

--- zero/index.vue
+++ zero/index.vue
@@ -1,3 +1,4 @@
 Sketch Files: "https://digitalax.mypinata.cloud/ipfs/QmXdWKKu4SPKzBnpNx2Ci391jeAHeb34mJut7A61M5Pjay",
 Blender File: "https://digitalax.mypinata.cloud/ipfs/QmfP8qVVATFujPgB7J8HtnedKgijKQopxyrjkNru7vsHVz",
-Image Render: "https://digitalax.mypinata.cloud/ipfs/QmVdoJ9iX1cbxUxqYV5gdGDzD7EkicJqC7UDfvPtWiJBGc"
+Image Render: "https://digitalax.mypinata.cloud/ipfs/QmVdoJ9iX1cbxUxqYV5gdGDzD7EkicJqC7UDfvPtWiJBGc",
+Blur Render: "https://digitalax.mypinata.cloud/ipfs/QmSkPgHoHjoVTqGqWDSdzQiXeh5LvZMgCmPwwfAZ2cu7US"
