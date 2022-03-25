53e506690344a3dfee89913e8f0ac56625df3aed
Emma-Jane MacKinnon-Lee <72898465+emmajane1313@users.noreply.github.com>
2022-03-25 08:27:08 -0400

    add

--- zero/index.vue
+++ zero/index.vue
@@ -5,21 +5,22 @@ Materials Zip File: "https://digitalax.mypinata.cloud/ipfs/QmbJoSF34qCNtsvaSwK3e
 Image Render: "https://digitalax.mypinata.cloud/ipfs/QmVdoJ9iX1cbxUxqYV5gdGDzD7EkicJqC7UDfvPtWiJBGc",
 Blur Render: "https://digitalax.mypinata.cloud/ipfs/QmSkPgHoHjoVTqGqWDSdzQiXeh5LvZMgCmPwwfAZ2cu7US"
 
+//Zero's Accessories:
 
 //Cypherline Gloves:
 Sketch Files: "https://digitalax.mypinata.cloud/ipfs/QmVKLvefL8EyRpqgPVgDyd2ZVoZUBXKUKQsBj529WWbMi9",
 ZPAC File: "https://digitalax.mypinata.cloud/ipfs/QmSpHLkFkt1m2wbjwQugTYEmrnmzEaNtq5YUtFxZyfF1yG",
 Blender File: "https://digitalax.mypinata.cloud/ipfs/QmbXme4WLeQXKoz21TQuM9YczJitYChBHsekpmHuGknHV6",
-Render Zip File: 
-
+Film Render File: "https://digitalax.mypinata.cloud/ipfs/",
+Image Render Zip File: "https://digitalax.mypinata.cloud/ipfs/",
 Diagram Sketch File: "https://digitalax.mypinata.cloud/ipfs/QmRXKL2cNEr3yEYH2oky4ijCXq497tVuS2Ah5pr7M4PmqZ",
 Diagram Render File: "https://digitalax.mypinata.cloud/ipfs/QmPVQ1Fqnc24FnpPcJFf1jyKwnWyh5PfQqBoXkwxvcMc2Y",
 
 //Cypherline Lenses:
 Sketch Files: "https://digitalax.mypinata.cloud/ipfs/QmbiWJHgRGM7SbibgaZvpvzkKhRd1LudYEnsdM6JPMLDX5",
 Blender File: "https://digitalax.mypinata.cloud/ipfs/QmNujBJJmLcXKK2DFoYNPn7jNzSaSsZHbKnXmNAx9MRLDN",
-Render Zip File: 
-
+Film Render File: "https://digitalax.mypinata.cloud/ipfs/QmXXhRApBtrw6AvoAXPwVYgepGiopTukhYvEY3QCcssYig",
+Image Render Zip File: "https://digitalax.mypinata.cloud/ipfs/",
 Diagram Sketch File: "https://digitalax.mypinata.cloud/ipfs/QmbuwCPPiSdpYiohH3csjqFzpXjVALvRwYTK2Xq5w2ZzoC",
 Diagram Render File: "https://digitalax.mypinata.cloud/ipfs/QmT767bmCxyUgacZs1fV9hPHR3TPK46DtPim3GR8SXXfpP",
 
