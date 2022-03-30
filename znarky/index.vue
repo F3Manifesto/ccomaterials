ed17841961378f91977feaf88ee16f6cb548b904
Emma-Jane MacKinnon-Lee <72898465+emmajane1313@users.noreply.github.com>
2022-03-30 08:29:01 -0400

    add

---
+++ znarky/index.vue
@@ -0,0 +1,4 @@
+Mixture of Znarky Assets that is Continuously Updated Overtime:
+SVG Image (Sitting Pose Front On): "https://digitalax.mypinata.cloud/ipfs/QmUSNcHsb5GHnUm9oMhyCaPewhXrVESxSWwmpZmL71jL7E",
+SVG Image (Standing with Skateboard): "https://digitalax.mypinata.cloud/ipfs/QmfWHuriTADNXRkbrYxTQHeRczs4ust2Nt3ke6q8ngDneC",
+SVG Image (Sitting Pose Side On): 
