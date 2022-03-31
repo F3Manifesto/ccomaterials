929310f17922db5cd22ff0f3f7780ff6b613aa2f
Emma-Jane MacKinnon-Lee <72898465+emmajane1313@users.noreply.github.com>
2022-03-31 18:45:22 -0400

    Update index.vue

--- d13a/index.vue
+++ d13a/index.vue
@@ -1,5 +1,3 @@
-Dezinformatsiya Text mp4: "https://digitalax.mypinata.cloud/ipfs/QmTALPyGnGEo1PtH3We7bznmXAf9XSK54EwGSSuei3zrwk",
-
 Skirt/Dress CLO File: "https://digitalax.mypinata.cloud/ipfs/QmYy7DxTayJTBEHmTLJwEMifgq5ofqpqbsCP7mz1rhizqv",
 Skirt/Dress Blender File: "https://digitalax.mypinata.cloud/ipfs/Qmbo3hEFFqKfSLYQ5YCbXkdbEWD99mXngKoKKhun1Y17bh",
 
@@ -16,13 +14,19 @@ Skirt/Dress Video Edit Blender File: "https://digitalax.mypinata.cloud/ipfs/QmZ2
 Skirt/Dress Composited File mp4: "https://digitalax.mypinata.cloud/ipfs/QmQv9L4stqub6tLpmeb2ezV6bk6FuX5cSqz3BStNb9gxHQ",
 
 Ruffles Video Edit Blender File: "https://digitalax.mypinata.cloud/ipfs/Qma16w43J3zAMKJr8ECRfBBdmAbo7aPKk64AfCy4F39DEg",
-Ruffles Composited File mp4: "https://digitalax.mypinata.cloud/ipfs/",
+Ruffles Composited File mp4: "https://digitalax.mypinata.cloud/ipfs/QmZEH3sSWyWhXVyt8pBKYMKTdkyEGm5NV8vFsM7PGniuKx",
 
 Heart Pants + Jacket Video Edit Blender File: "https://digitalax.mypinata.cloud/ipfs/QmQNXAHkwVuHtdnLewEe614i964BPNwCe5YPpU3gMineut",
-Heart Pants + JacketComposited File mp4: "https://digitalax.mypinata.cloud/ipfs/",
+Heart Pants + Jacket Composited File mp4: "https://digitalax.mypinata.cloud/ipfs/QmXL2M61egiP3g2CGC9gipNry9WXuWf9FWinvV89dzxZer",
 
 Heart Pants + Crop Top Video Edit Blender File: "https://digitalax.mypinata.cloud/ipfs/QmSVm6vZtsiqL2Pn2jUqJEpjPJgzF1GvgPPTAR8oBpq3tC",
 Heart Pants + Crop Top Composited File mp4: "https://digitalax.mypinata.cloud/ipfs/",
+
+Dezinformatsiya Text mp4: "https://digitalax.mypinata.cloud/ipfs/QmTALPyGnGEo1PtH3We7bznmXAf9XSK54EwGSSuei3zrwk",
+Dezinformatsiya Text Filter 1 mp4: "https://digitalax.mypinata.cloud/ipfs/QmRgoWYDQVeVJmeou7kpPJ331ai9QvBmpme72hDFL8stAK",
+Dezinformatsiya Text Filter 2 mp4: "https://digitalax.mypinata.cloud/ipfs/QmecDDoBgz8tt8mYZ7eYovTSMWVrVF3TxJmp9zYabK3TfM",
+Dezinformatsiya Text Filter 3 mp4: "https://digitalax.mypinata.cloud/ipfs/QmXv85BCPy7CWoHsa5rR9PfdCJfB9wQgficxBszizj21Mn",
+Dezinformatsiya Text Filter 4 mp4: "https://digitalax.mypinata.cloud/ipfs/Qmb89RrexY2MN8HFNGQ5cY6Z33GFy6SjToVSa4dmpVMhTq",
 
 Final Render File: "https://digitalax.mypinata.cloud/ipfs/",
 Final Render File w/ Audio: "https://digitalax.mypinata.cloud/ipfs/"                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
