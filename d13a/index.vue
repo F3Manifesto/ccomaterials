3311ed3f47dea1c0097c41941e71068b44d942a4
Emma-Jane MacKinnon-Lee <72898465+emmajane1313@users.noreply.github.com>
2022-03-31 15:24:20 -0400

    add

--- d13a/index.vue
+++ d13a/index.vue
@@ -1,1 +1,28 @@
+Dezinformatsiya Text mp4: "https://digitalax.mypinata.cloud/ipfs/QmTALPyGnGEo1PtH3We7bznmXAf9XSK54EwGSSuei3zrwk",
 
+Skirt/Dress CLO File: "https://digitalax.mypinata.cloud/ipfs/QmYy7DxTayJTBEHmTLJwEMifgq5ofqpqbsCP7mz1rhizqv",
+Skirt/Dress Blender File: "https://digitalax.mypinata.cloud/ipfs/Qmbo3hEFFqKfSLYQ5YCbXkdbEWD99mXngKoKKhun1Y17bh",
+
+Ruffles CLO File: "https://digitalax.mypinata.cloud/ipfs/QmckWMkkSejwqNxux1pCXapbrpv2Gt2LX8WppyTBzJncd2",
+Ruffles Blender File: "https://digitalax.mypinata.cloud/ipfs/QmWRmkcAV57gV976EcxzRtUe2qGBbvGfut8xwYifFrjRg7",
+
+Heart Pants + Jacket CLO File: "https://digitalax.mypinata.cloud/ipfs/QmV9gF8xGQZEyQDi9a79Z5UDmtZfz1ZtLX9BP1sWe18RM6",
+Heart Pants + Jacket Blender File: "https://digitalax.mypinata.cloud/ipfs/QmW5oCHwAdAsCAKz72xTViXjpLXJuTMseCQZF3VQ7AJcHA",
+
+Heart Pants + Crop Top CLO File: "https://digitalax.mypinata.cloud/ipfs/QmRoHoDvmJ4wKq91pxngBo8cpagfQG9HgLNQswCGPpKNFR",
+Heart Pants + Crop TopBlender File: "https://digitalax.mypinata.cloud/ipfs/QmQzfdgYSNr9w8ovpZ92wqyC2KeU4BJnFVwPZLkgcBQ9ht",
+
+Skirt/Dress Video Edit Blender File: "https://digitalax.mypinata.cloud/ipfs/QmZ2hsHhkQEbKi7zAGzs5SyK8hh9dY5rNgDMcLr6uTf6T2",
+Skirt/Dress Composited File mp4: "https://digitalax.mypinata.cloud/ipfs/QmQv9L4stqub6tLpmeb2ezV6bk6FuX5cSqz3BStNb9gxHQ",
+
+Ruffles Video Edit Blender File: "https://digitalax.mypinata.cloud/ipfs/",
+Ruffles Composited File mp4: "https://digitalax.mypinata.cloud/ipfs/",
+
+Heart Pants + Jacket Video Edit Blender File: "https://digitalax.mypinata.cloud/ipfs/",
+Heart Pants + JacketComposited File mp4: "https://digitalax.mypinata.cloud/ipfs/",
+
+Heart Pants + Crop Top Video Edit Blender File: "https://digitalax.mypinata.cloud/ipfs/",
+Heart Pants + Crop Top Composited File mp4: "https://digitalax.mypinata.cloud/ipfs/",
+
+Final Render File: "https://digitalax.mypinata.cloud/ipfs/",
+Final Render File w/ Audio: "https://digitalax.mypinata.cloud/ipfs/"                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
