4679719153506733207b15759cac0a7072b6d57e
Emma-Jane MacKinnon-Lee <72898465+emmajane1313@users.noreply.github.com>
2022-04-03 06:10:12 -0400

    add

--- znarky/comic-panel/index.vue
+++ znarky/comic-panel/index.vue
@@ -1,2 +1,3 @@
-SVG File Poignant Guide: "https://digitalax.mypinata.cloud/ipfs/QmdfZ7Z2dNfJMyDkCHSQQPpLVc7DiEXPiW7MEkU5fNGVzn"
+SVG File Poignant Guide: "https://digitalax.mypinata.cloud/ipfs/QmdfZ7Z2dNfJMyDkCHSQQPpLVc7DiEXPiW7MEkU5fNGVzn",
+SVG File Poignant Guide Extended: "https://digitalax.mypinata.cloud/ipfs/QmTCC4vUmdatsR8W1doFFBp2gwEYis6Y5f5xfncM7KDrmr",
 SVG File Empty Closet: "https://digitalax.mypinata.cloud/ipfs/QmdfZ7Z2dNfJMyDkCHSQQPpLVc7DiEXPiW7MEkU5fNGVzn"
