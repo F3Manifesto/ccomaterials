1e2bb703c5b023b265da7d3b3fcadd6190c294e9
Emma-Jane MacKinnon-Lee <72898465+emmajane1313@users.noreply.github.com>
2022-04-02 22:44:14 -0400

    add

---
+++ edit/index.vue
@@ -0,0 +1,5 @@
+// Edit:
+Sketch Files: "https://digitalax.mypinata.cloud/ipfs/QmVQcSfQJJcLyDhnwinopAjobseJp2MrdVX2ousW96D4Us",
+Blender File: "https://digitalax.mypinata.cloud/ipfs/",
+Materials Zip File: "https://digitalax.mypinata.cloud/ipfs/",
+Image Render: "https://digitalax.mypinata.cloud/ipfs/",
