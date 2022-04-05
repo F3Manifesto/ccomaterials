23dbd6b09fd4b65c4fd0cd47bad1d47e9bc308ca
Emma-Jane MacKinnon-Lee <72898465+emmajane1313@users.noreply.github.com>
2022-04-05 01:31:09 -0400

    Update index.vue

--- edit/index.vue
+++ edit/index.vue
@@ -2,4 +2,4 @@
 Sketch Files: "https://digitalax.mypinata.cloud/ipfs/QmVQcSfQJJcLyDhnwinopAjobseJp2MrdVX2ousW96D4Us",
 Blender File: "https://digitalax.mypinata.cloud/ipfs/QmNYtZYsUEgSudmtDvxdA9BaHbU9iJ8tVXfaqduKpeS5Vu",
 Materials Zip File: "https://digitalax.mypinata.cloud/ipfs/QmP1qgeLtwetU3AYg9XpYVeyuPatgADa5aNSgw17YKwJv7",
-Image Render: "https://digitalax.mypinata.cloud/ipfs/",
+Image Render: "https://digitalax.mypinata.cloud/ipfs/QmWcVEAeuoc2gpHBGGCfXSzCMFW32yT96MtPQRtQhxHHyr",
