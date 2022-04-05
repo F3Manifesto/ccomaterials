8bd4c38c47b207dcf9592aba5efba271e282c70b
Emma-Jane MacKinnon-Lee <72898465+emmajane1313@users.noreply.github.com>
2022-04-05 10:46:41 -0400

    add

--- edit/index.vue
+++ edit/index.vue
@@ -2,4 +2,31 @@
 Sketch Files: "https://digitalax.mypinata.cloud/ipfs/QmVQcSfQJJcLyDhnwinopAjobseJp2MrdVX2ousW96D4Us",
 Blender File: "https://digitalax.mypinata.cloud/ipfs/QmNYtZYsUEgSudmtDvxdA9BaHbU9iJ8tVXfaqduKpeS5Vu",
 Materials Zip File: "https://digitalax.mypinata.cloud/ipfs/QmP1qgeLtwetU3AYg9XpYVeyuPatgADa5aNSgw17YKwJv7",
-Image Render: "https://digitalax.mypinata.cloud/ipfs/QmWcVEAeuoc2gpHBGGCfXSzCMFW32yT96MtPQRtQhxHHyr",
+Image Render: "https://digitalax.mypinata.cloud/ipfs/QmT2rBJ4rBEJShLciUsoWMXvWzEgDtEADLqzxt9JgErxB3",
+
+//Edit's Accessory Pack:
+
+
+//Fuzzy Hat:
+Sketch Files: "https://digitalax.mypinata.cloud/ipfs/",
+Blender File: "https://digitalax.mypinata.cloud/ipfs/",
+Film Render File: "https://digitalax.mypinata.cloud/ipfs/",
+Image Render File: "https://digitalax.mypinata.cloud/ipfs/QmWcVEAeuoc2gpHBGGCfXSzCMFW32yT96MtPQRtQhxHHyr",
+Diagram Sketch File: "https://digitalax.mypinata.cloud/ipfs/",
+Diagram Render File: "https://digitalax.mypinata.cloud/ipfs/",
+
+//Edit w/ Fuzzy Hat:
+Blender File: "https://digitalax.mypinata.cloud/ipfs/",
+Render File: "https://digitalax.mypinata.cloud/ipfs/",
+
+//Earrings:
+Sketch Files: "https://digitalax.mypinata.cloud/ipfs/",
+Blender File: "https://digitalax.mypinata.cloud/ipfs/",
+Film Render File: "https://digitalax.mypinata.cloud/ipfs/",
+Image Render File: "https://digitalax.mypinata.cloud/ipfs/",
+Diagram Sketch File: "https://digitalax.mypinata.cloud/ipfs/",
+Diagram Render File: "https://digitalax.mypinata.cloud/ipfs/",
+
+//Edit w/ Earrings:
+Blender File: "https://digitalax.mypinata.cloud/ipfs/",
+Render File: "https://digitalax.mypinata.cloud/ipfs/",
