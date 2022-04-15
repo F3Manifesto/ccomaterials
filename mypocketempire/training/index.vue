0d4aaa5c4944607cc9fd22841a1abfe8f558a70d
Emma-Jane MacKinnon-Lee <72898465+emmajane1313@users.noreply.github.com>
2022-04-15 19:49:32 -0400

    add files

---
+++ mypocketempire/training/index.vue
@@ -0,0 +1,43 @@
+// Training and homepage Aseprite Files
+: , 
+Fox&Aro Aseprite: "https://f3manifesto.mypinata.cloud/ipfs/Qmc2Tx1FJc2gB9jypwnVuCRFPcWXU8VNFrbxLWCUHAzKAF",
+Background Opening: "https://f3manifesto.mypinata.cloud/ipfs/QmSjpUjBU3WGVTWsL5K7mpuQPrR1mNDLR38oZCd5RHR36x",
+Background Training: "https://f3manifesto.mypinata.cloud/ipfs/QmREVhuWuaMi2ZjU9og7VeErUQz8whSqboonZkwuBzRLVj",
+Floating Island: "https://f3manifesto.mypinata.cloud/ipfs/Qma8VZXC3zmBmLxnf7LJoQTZ3czTcBHhrbnTcGLL5JesKc",
+Title: "https://f3manifesto.mypinata.cloud/ipfs/QmX1U9YB7m2SA9UABzuYnoRaqETZoP9Q4MuZ8zMyU7yVpR",
+Play: "https://f3manifesto.mypinata.cloud/ipfs/QmXjXQanGFUX3xHqsTcZW53A9VUJ1LLw7jyyUT8pu6ctPs",
+Train: "https://f3manifesto.mypinata.cloud/ipfs/QmWhN1ZYhPyi1VJS5q2CZPGfN8p59N7J3EshvBxbnnUi74",
+Play Again: "https://f3manifesto.mypinata.cloud/ipfs/QmasqwzAfhMpJGS8apQ7pey9kTFMtQDjE9fQ3Nn6Rxyh2H",
+Health Bar Heart: "https://f3manifesto.mypinata.cloud/ipfs/QmStCap9HHhDjG3a3XQXG2FfzpsUaXy6RD6S7Jh7tJ8kuT",
+Run Story: "https://f3manifesto.mypinata.cloud/ipfs/QmaQUwi2AFB9hSEa2g2SXyGhdEUPh5QWQB9z1XxXycvEeF",
+Return: "https://f3manifesto.mypinata.cloud/ipfs/QmZW8riZwpq2LmGR2KyzJrVq57mYGsJUSMN2yBRN1zHMQa",
+
+// Aro
+Idle Aro: "https://f3manifesto.mypinata.cloud/ipfs/QmZnnxWVMaXicf2y6Y48BhkuWQuSbkP5E6FNUawGTWnKPS",
+Jump & Fall Aro: "https://f3manifesto.mypinata.cloud/ipfs/QmcDBcpgZDNUaFtGi9KN1nTqT36mMAJAaAwzh5VuiprXxg",
+Attack Aro; "https://f3manifesto.mypinata.cloud/ipfs/Qmb1p2HevUdaNmKf26Lm4wCw79T1m5iiV6GjfgsXjUV7v6"
+Take Hit Aro: "https://f3manifesto.mypinata.cloud/ipfs/QmQQ7nALfxw9ksQEqpMHDgx8K9bPKx5isNvv7imxFdyThx",
+Death Aro: "https://f3manifesto.mypinata.cloud/ipfs/Qmcbcn5Udi8n1aigqLESEx7PfaWHVZkCZJ8YrkMEE54ypF",
+Sword: "https://f3manifesto.mypinata.cloud/ipfs/QmY6fATytpUZKzmEMMoY3Jv7QjTdPbD4Fd3W1ARz3Epqzh",
+Run Aro: "https://f3manifesto.mypinata.cloud/ipfs/QmRZcmKBXtomyMxx8oMFZxApJxcemq6uJMUhHGUTPQKwH6",
+
+
+// Wizard
+Idle Wizard: "https://f3manifesto.mypinata.cloud/ipfs/QmWGrmVAT99smF7X8rYvTFEsMVGiBBZrH6Wn33hSTNKrZv",
+Jump Wizard: "https://f3manifesto.mypinata.cloud/ipfs/QmZtUgtkJZR11BxoEPDnVGwvaUP3KaauxwzQfY4VWAkx5E",
+Take Hit Wizard: "https://f3manifesto.mypinata.cloud/ipfs/QmRwzDUPFiJMom6AjaKukzsPKt1do3XXUKZ5tApogRphjk",
+Death Wizard: "https://f3manifesto.mypinata.cloud/ipfs/QmUiD5b35aaPAXzYNfEZemEebMoDYeeNzQ5qzNwhDogb6r",
+Fall Wizard: "https://f3manifesto.mypinata.cloud/ipfs/QmZ5td4Xvc6zhxe5XdxCTjnzFT16eAN7c694hSmFkGFQxn",
+Run Wizard: "https://f3manifesto.mypinata.cloud/ipfs/QmRb6qk5yAbG3mDGgxQza5Y4n1N22A5aXJBEnJVMeZP41V",
+Attack Wizard: "https://f3manifesto.mypinata.cloud/ipfs/QmNkmF1Ce53vSCZBw9WynxfQZ9idGRy3QKTZJgAVwnfn4j"
+
+
+// Opening Story
+Scene One: "https://f3manifesto.mypinata.cloud/ipfs/QmQV3JutKNxyHwzuPdzzAgU1N4sN4o68rrWZ7zAxXNS6HZ",
+Scene Two: "https://f3manifesto.mypinata.cloud/ipfs/QmSpAuW5Xasizb7paUEYzXr1GNbpXvPmDnzsrTwvFNqrZA",
+Scene Three: "https://f3manifesto.mypinata.cloud/ipfs/QmZn3CxkqcLPtjmgqnfmrdJESueLdajWeEv5BKLUwqxr5m",
+Scene Four: "https://f3manifesto.mypinata.cloud/ipfs/QmaaeXi79fpfvdj8Kuj1at39rkqLwgppU7kL1rVwicDhxa",
+Scene Five: "https://f3manifesto.mypinata.cloud/ipfs/Qmbw88UvtLFqnoUxLNvbz7rLiXf4i9WhGcGXfLeuJLwP7b",
+
+
+
