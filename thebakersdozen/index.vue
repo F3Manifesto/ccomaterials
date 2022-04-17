948bc3b556d10a0bf51c8aca0801dfbc8d4f7574
Emma-Jane MacKinnon-Lee <72898465+emmajane1313@users.noreply.github.com>
2022-04-16 21:51:04 -0400

    add

--- thebakersdozen/index.vue
+++ thebakersdozen/index.vue
@@ -1,23 +1,23 @@
 TBD #1 Blender File: "https://digitalax.mypinata.cloud/ipfs/QmNepLDr7fsPqifh89QG4B1c61WwJd5dttbkR4Au5bYbSj",
-TBD #1 Render File: "https://digitalax.mypinata.cloud/ipfs/",
+TBD #1 Render File: "https://digitalax.mypinata.cloud/ipfs/QmQ6iUm1zM57mXw8WahaYWWy3cUoBaQiLPUiCT6ttPSRiQ",
 
 TBD #2 Blender File: "https://digitalax.mypinata.cloud/ipfs/QmXY1Lso97TmVKgR661ZLjT72yowuPMaj6FBEy1qE32EBx",
-TBD #2 Render File: "https://digitalax.mypinata.cloud/ipfs/",
+TBD #2 Render File: "https://digitalax.mypinata.cloud/ipfs/QmTngG2XVxZ6SjC4Wj2wptmH7HC7LGGcTaGcrj1V9vjzcy",
 
 TBD #3 Blender File: "https://digitalax.mypinata.cloud/ipfs/QmW6ULCTVNufQXVeqRhspMSgguXYUMikdtsmWm4ASPzPGy",
-TBD #3 Render File: "https://digitalax.mypinata.cloud/ipfs/",h",
+TBD #3 Render File: "https://digitalax.mypinata.cloud/ipfs/QmfUMvHZSra17ra6FqPXQ4hvPZPia3qEy6RuFx1tpBfdsp",
 
 TBD #4 Blender File: "https://digitalax.mypinata.cloud/ipfs/QmRm7x1sDPEBUmyeHkns6eHoAEizRxSn9EnySuZ5NjMhUm",
-TBD #4 Render File: "https://digitalax.mypinata.cloud/ipfs/",
+TBD #4 Render File: "https://digitalax.mypinata.cloud/ipfs/QmXHJtk7MdnFJG1JhiLTnb7xpcyrmLGQxNjvUwLGiTKXEJ",
 
 TBD #5 Blender File: "https://digitalax.mypinata.cloud/ipfs/QmYsr1gxdoM6g9v5nY5ZAiEo7KVDt6mNRBPYH3AT8mPRUX",
-TBD #5 Render File: "https://digitalax.mypinata.cloud/ipfs/",
+TBD #5 Render File: "https://digitalax.mypinata.cloud/ipfs/QmTFUjPK4ZcZ3wMcSVGrpeLSC1X41Xb4vBZtjepMHH5AJ5",
 
 TBD #6 Blender File: "https://digitalax.mypinata.cloud/ipfs/QmWQwYwSjEUcMNfQKDY8MPC9YbRfkYLKbAHqgNBf2fVMdw",
-TBD #6 Render File: "https://digitalax.mypinata.cloud/ipfs/",
+TBD #6 Render File: "https://digitalax.mypinata.cloud/ipfs/QmdtyMEGQAPHk6oCYajhQWJH922wayZigR2aavTZcKWskf",
 
 TBD #7 Blender File: "https://digitalax.mypinata.cloud/ipfs/QmTVxHdkT2arpkx9VJPMUPXBSwBaCqTJbje9YzxEW8LjtA",
-TBD #7 Render File: "https://digitalax.mypinata.cloud/ipfs/",
+TBD #7 Render File: "https://digitalax.mypinata.cloud/ipfs/QmSS78NDMeTRRq1aXtnSTDMNTTptvu9XTPS4XKgQnrw1jw",
 
 TBD #8 Blender File: "https://digitalax.mypinata.cloud/ipfs/QmdeNgVfhxUzWNnUrphL3cphSJkRhCfTdcnABX8o9GVVLS",
-TBD #8 Render File: "https://digitalax.mypinata.cloud/ipfs/",
+TBD #8 Render File: "https://digitalax.mypinata.cloud/ipfs/QmQ88aENEd8ZGJPT346BAP1VKPvDFUXBMdocJoeHBzobA9",
