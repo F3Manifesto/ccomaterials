8aa1a23fcf08981f3af227dba4b38e92675941c1
Emma-Jane MacKinnon-Lee <72898465+emmajane1313@users.noreply.github.com>
2022-04-24 07:34:35 -0400

    add

--- mypocketempire/allhistorylost/index.vue
+++ mypocketempire/allhistorylost/index.vue
@@ -1,1 +1,39 @@
 
+// Aseprite Scenes
+Scene Zero: "https://f3manifesto.mypinata.cloud/ipfs/QmZrBxFzWkv1CTGpPbm8hsgkGRFekHiN5Pnw2UiV273iE1",
+Scene One: "https://f3manifesto.mypinata.cloud/ipfs/Qmcrdb67rwfKxmw2hGcdBbSAkafLR35bXuVkDAB3BqndzJ",
+Scene Two: "https://f3manifesto.mypinata.cloud/ipfs/QmdqAVWEwcQcHcQuMo7EXd8nLGeFFikMZXE7MzUFcDsokx",
+Scene Three: "https://f3manifesto.mypinata.cloud/ipfs/QmbqPZX6ondnZ22QLjag9VsFARGAq8d9VNNzuDJhZftWKV",
+Scene Four: "https://f3manifesto.mypinata.cloud/ipfs/QmQLv7ekHBwqqbuQem8KVqPd1Qj3pv3HuoW77Zju2tBoVW"
+Scene Five: "https://f3manifesto.mypinata.cloud/ipfs/Qmazh4wtF5vhYJjMm2QFpMuRUvUp6ME3hp3ueDREcVaCTx"
+Scene Six: "https://f3manifesto.mypinata.cloud/ipfs/QmTorYQ1HeQEtA7dyttY4iibLiPZqaKpN1JSCRPxqjv9ws"
+Scene Seven: "https://f3manifesto.mypinata.cloud/ipfs/QmT5a149Y8dBKxxWYb5wrQg7KtyTK7x5jjf7BhJ2BPMVGT"
+Scene Eight: "https://f3manifesto.mypinata.cloud/ipfs/Qmcp3aLFwnq4VQmreq7jcSpNtEDZ6BjRuby6PKNU7XARJE"
+Scene Nine: "https://f3manifesto.mypinata.cloud/ipfs/QmViJzT44WiouQJyZMLMcwaeAhpRecVQ5BcB9GDzqPTUpW"
+Scene Ten: "https://f3manifesto.mypinata.cloud/ipfs/QmWkojUT9e9pRc6ZrNbzQrmDYmqVaJnCk6enA5UBn2omJS"
+Scene Eleven: "https://f3manifesto.mypinata.cloud/ipfs/QmaTDkjf35kqhrZWnc8v451uXdU6cznhgfFCTSi9bJjVm7"
+Scene Twelve: "https://f3manifesto.mypinata.cloud/ipfs/Qmd3AbokErXTUY3Gw98o1xW6AzbVeyvntAiB2FjFrqmFpc"
+Scene Thirteen: "https://f3manifesto.mypinata.cloud/ipfs/QmXgyVQt98UnFdbm9xFSE6zM7bUMQFoCyz48VbYdUgL1FN"
+Scene Fourteen: "https://f3manifesto.mypinata.cloud/ipfs/QmevYh9JL4ur3aBtPmeyxKWcRRezdKKHrCBEqnLoa31mSR"
+Scene Fifteen: "https://f3manifesto.mypinata.cloud/ipfs/QmXfDPwWie4vDAH6xRJ7xKBN2YXjEFgpGd3uCnZmxMWpVM"
+Scene Sixteen: "https://f3manifesto.mypinata.cloud/ipfs/QmfUsr6b8wiCDDcW8kcMZR2bLeGztGEvT8TpH1so4q6278"
+Scene Seventeen: "https://f3manifesto.mypinata.cloud/ipfs/QmbuVbZejTr7WkTDxqFKE8RK9PV2N2GnTpLWDHmrjvmrcq"
+Scene Eighteen: "https://f3manifesto.mypinata.cloud/ipfs/QmW8jwVWToxqMWeqQkLD6h44QVBSQLN3yfr5S9Rt1RTZ9e"
+Scene Nineteen: "https://f3manifesto.mypinata.cloud/ipfs/QmTbU7EK9JVK293yf1qNUUCdfYqGs7NoE2TyBnGwux4pww"
+Scene Twenty: "https://f3manifesto.mypinata.cloud/ipfs/QmdnQBVzafBAJ6KAFJXuij2t8RvwB4zoBhVU3KxxJzrHec"
+Scene Twenty One: "https://f3manifesto.mypinata.cloud/ipfs/QmcVtBYSghNG3S2uYurj2Q8KgdMPSRHoAnbg8uSHwAncfB"
+Scene Twenty Two: "https://f3manifesto.mypinata.cloud/ipfs/QmeRySXYtgutrdLfXmr8E1R3brNArpFCR4gBwKAkSRKyyX"
+Scene Twenty Three: "https://f3manifesto.mypinata.cloud/ipfs/QmYZ3DjVnHE9EwDfx7CmLoLbF6UadGdWG5MJLfnohA7KHN"
+Scene Twenty Four: "https://f3manifesto.mypinata.cloud/ipfs/QmWWcKbS263LVMo7ojTTTb8gAiuKGgLYbW11bfUP5uE3y8"
+Scene Twenty Five: "https://f3manifesto.mypinata.cloud/ipfs/QmeQ7U7oJGo3MFBnTiEEnrMBMJzQ33QGN85nfw3kKTxyYw"
+Scene Twenty Six: "https://f3manifesto.mypinata.cloud/ipfs/QmUHortRjaZPQBgffEtwVsVBi57v2ftPpcpQ8pTKN24Unp"
+Scene Twenty Seven: "https://f3manifesto.mypinata.cloud/ipfs/QmNd6CWQjxgNXxrhQdXYLPRki4BctpxAPH5djzVwiFp55b"
+
+
+// Music Ableton File
+Track One: "https://f3manifesto.mypinata.cloud/ipfs/QmRXnkwTcZ9PjtMQW6M7mvbqEamr8ucRP9DFHVirARjwj6"
+Track Two: "https://f3manifesto.mypinata.cloud/ipfs/QmP9utTysr5r687uc5aRX5SimucsaPJ6QaEREzoP73XTsJ"
+
+
+// Final Render
+Final Render: "https://f3manifesto.mypinata.cloud/ipfs/QmWZaXCWQ72Nmtwpz4aGoXn8h1ViKD2Qi2KQw6J2QZpV1x"
