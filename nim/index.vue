9f1d3ed75e140084627d04c4281d27344df52a3b
Emma-Jane MacKinnon-Lee <72898465+emmajane1313@users.noreply.github.com>
2022-04-28 10:26:43 -0400

    add

---
+++ nim/index.vue
@@ -0,0 +1,9 @@
+// Nim
+Blender File: "https://digitalax.mypinata.cloud/ipfs/Qmd98RwJKnTyRZAv6FiBajfVYH8P3KZiKrfoXhSwHWJp7b",
+Material Image Files Zip: "https://f3manifesto.mypinata.cloud/ipfs/QmVpgXwAokzoyjMgJutR3jw8Hrax7StgXJJuZeQUYQgJz4",
+Render File: "https://f3manifesto.mypinata.cloud/ipfs/QmNdJ9mFhjRNGcxrAEgguDZ5JeSyF6S89HsFKdeJxjnJ1w",
+
+
+// Nim's Pose
+Blender File: "https://f3manifesto.mypinata.cloud/ipfs/",
+Render File: "https://f3manifesto.mypinata.cloud/ipfs/Qma2JEGrfivFAWPj9L2y6PsahXc4aTT3ogg6H4VR1M6GLu"
