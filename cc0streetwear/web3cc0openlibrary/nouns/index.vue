570083f838d68d05be947d246b0211ccea65736c
Emma-Jane MacKinnon-Lee <72898465+emmajane1313@users.noreply.github.com>
2022-04-29 01:38:02 -0400

    add

--- cc0streetwear/web3cc0openlibrary/nouns/index.vue
+++ cc0streetwear/web3cc0openlibrary/nouns/index.vue
@@ -1,1 +1,4 @@
 
+// Nouns Hoodies Sketch File: "https://f3manifesto.mypinata.cloud/ipfs/QmNubSZ9ctWLqvxn8wazjjnJBN9MRa43vAj1TYtSkN31HV".
+// Nouns Shirts Sketch File: "https://f3manifesto.mypinata.cloud/ipfs/QmZVWK587TzzzZrWPJaLDeeBrjXJe2kRYnUBejmLLtqvav",
+// Image Sketch File: "https://f3manifesto.mypinata.cloud/ipfs/QmdEyr8qAihEvhQmci2HLeotraQs6Ct5beUqwyM86sAAVg"
