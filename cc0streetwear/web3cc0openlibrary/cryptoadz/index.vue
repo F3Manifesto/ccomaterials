6d4866f83344bb0211ebe8a62a321b635df8d0c9
Emma-Jane MacKinnon-Lee <72898465+emmajane1313@users.noreply.github.com>
2022-04-30 21:11:29 -0400

    add

--- cc0streetwear/web3cc0openlibrary/cryptoadz/index.vue
+++ cc0streetwear/web3cc0openlibrary/cryptoadz/index.vue
@@ -1,5 +1,5 @@
 // Sketch File Hoodies: "https://f3manifesto.mypinata.cloud/ipfs/QmTG2FwSFe6nA67ExdD9YQ6aUy6xCPdQJGtEGw6n8X27zE",
 
-// Sketch File Shirt: "",
+// Sketch File Shirt: "https://f3manifesto.mypinata.cloud/ipfs/QmTu5BQsVfUar9p92SaCPyjYZEAKaey2HiPA1yytdQFgsU",
 
 // Image Panel Files: "https://f3manifesto.mypinata.cloud/ipfs/QmVk97jYGppFpuKS5dT3zz2Qi2T7Y3qrGVSZfVFcJtX1bx"
