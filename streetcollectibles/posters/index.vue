6629521fa7ce9ff442c9d2c1b03918836ed80bb2
Emma-Jane MacKinnon-Lee <72898465+emmajane1313@users.noreply.github.com>
2022-05-14 10:27:24 -0400

    add

---
+++ streetcollectibles/posters/index.vue
@@ -0,0 +1,7 @@
+Sketch File Posters:"https://f3manifesto.mypinata.cloud/ipfs/QmU1m6ANsa2UCkcCFeMsoAhyW6ZKChAriDrUuKZurH1mMz",
+After Effects K$D File: "https://f3manifesto.mypinata.cloud/ipfs/QmTBrsDeeJezQhxZvSi4fro92H8KRcKCCcgZmUfLhLcXFj",
+After Effects What is Web3 Really? File: "https://f3manifesto.mypinata.cloud/ipfs/QmTX5pji9R6wX25NdPivZg678AbwZsrGw6dDnRZBrU8Ak9",
+After Effects Always Demand for More Thread File: "https://f3manifesto.mypinata.cloud/ipfs/QmcJQY9QK99hDFXWc7kH6a6e2ppWQpSZxp3sgbHwZfNJiG",
+After Effects Web3 CC0 Open Library File: "https://f3manifesto.mypinata.cloud/ipfs/QmNe9DeLCW92GYcT1DZbr7fpDYhqoNUieUv6BWY4Dbk8Qq",
+After Effects My Pocket Empire File: "https://f3manifesto.mypinata.cloud/ipfs/QmdwBQMM2jTEeSUsoN98bb2kHAonhuEt8qrufWM9P9LvzM",
+After Effects Public-Private Keys File: "https://f3manifesto.mypinata.cloud/ipfs/QmY4M1FPrUa5Q43Jy3c3xfCgPhtZb13A89LZM35Gm9y8EC"
