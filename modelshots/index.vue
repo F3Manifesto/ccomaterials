0ada0644df9a58fd42d848d6b0640f49d66f319b
Emma-Jane MacKinnon-Lee <72898465+emmajane1313@users.noreply.github.com>
2022-05-15 21:55:31 -0400

    Create index.vue

---
+++ modelshots/index.vue
@@ -0,0 +1,3 @@
+May 2022 Models Photoshoot NYC Set One: "https://drive.google.com/drive/folders/1PzHFOm2Go4CFxQOgObO1thB0Xeyr0cnm",
+May 2022 Models Photoshoot NYC Set Two: "https://drive.google.com/drive/folders/1zZoF2Niu9N6_Uvvzg7B_ZDVFMrgeCSZu"
+Model Instagrams: @baebytree, @isac_mango, @javanmcnair, @nellielicul, @yourfavexwife, @allyhasim05 (twitter)
