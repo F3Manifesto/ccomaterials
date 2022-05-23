bfd45855a45bdd4a702fd2b4421d6b67225841d2
Emma-Jane MacKinnon-Lee <72898465+emmajane1313@users.noreply.github.com>
2022-05-23 09:20:36 -0400

    add

--- howtomakeitinweb3/index.vue
+++ howtomakeitinweb3/index.vue
@@ -2,3 +2,4 @@
 "Blender Scene File": https://f3manifesto.mypinata.cloud/ipfs/QmXnkPRN29dkD5HiCMXiN3vP29d7MJXx456wHK1EbWXSCA,
 "TV Movie File MP4": https://f3manifesto.mypinata.cloud/ipfs/QmYrVyzcSdD3XgRAXG1dBLG279SX7y2aGk3ViTP4Dj5Pgh,
 "TV Movie After Effects File": https://f3manifesto.mypinata.cloud/ipfs/QmWbmVN4N4ymGAu436htbWYxp7LaMySCdQNRnVSg7WUW6x,
+"TV Compoited Scene After Effects File w/ Music": https://f3manifesto.mypinata.cloud/ipfs/QmW3gAw8Tm8T2mdNeWnF3UUDJL881wr1dyrkg1ZJxLbDzV,
