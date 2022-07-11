8a4ede0df0e4a331f86e54f0e802b9053f44ce11
Emma-Jane MacKinnon-Lee <72898465+emmajane1313@users.noreply.github.com>
2022-07-11 15:05:20 -0400

    add

--- zkzeitgeist/index.vue
+++ zkzeitgeist/index.vue
@@ -7,3 +7,7 @@ Final Edited File: "https://digitalax.mypinata.cloud/ipfs/QmcKf5fXJ2pmZjicEn5DYr
 // Jump Gate #2
 Blender Animation File: "https://digitalax.mypinata.cloud/ipfs/QmXmv83XHoRRxBEK3xx1Lazfp7PGZQit5kDmGvWWSt5BPq",
 Render Animation File: "https://digitalax.mypinata.cloud/ipfs/QmNmSVQrocWYQKPS9wzz7Eb41CGgHNDLUs2qZKNpugBcL2",
+
+// Jump Gate #3
+AE File: "https://f3manifesto.mypinata.cloud/ipfs/QmSVgTQRGfaRkripQ9PNTi1q6WpN3EZnQeEd3ANev8HQFj",
+Render Animation File: "https://f3manifesto.mypinata.cloud/ipfs/Qmeh5RjAY6n17uibED4t5hkTBV6kqet3HmrwciRcZJE11E",
