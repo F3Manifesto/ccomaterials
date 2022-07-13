39fc679d75e3131c9a713f9666b7ad04f981165f
digitalax_ <72898465+digitalax1@users.noreply.github.com>
2022-07-13 18:01:26 -0400

    znarky fix

--- znarky/comic-panel/index.vue
+++ znarky/comic-panel/index.vue
@@ -1,3 +1,3 @@
 SVG File Poignant Guide: "https://digitalax.mypinata.cloud/ipfs/QmdfZ7Z2dNfJMyDkCHSQQPpLVc7DiEXPiW7MEkU5fNGVzn",
 SVG File Poignant Guide Extended: "https://digitalax.mypinata.cloud/ipfs/QmPrKbCvZpvNPBZqDaXnXGhXakQYA5ZyWHJ2gjKiJFFxHF",
-SVG File Empty Closet: "https://digitalax.mypinata.cloud/ipfs/QmdfZ7Z2dNfJMyDkCHSQQPpLVc7DiEXPiW7MEkU5fNGVzn"
+SVG File Empty Closet: "https://f3manifesto.mypinata.cloud/ipfs/QmQ6RyvtniAXjJcXGfAarufP4sBb3YEPPzPMbs7UpkQFkS"
