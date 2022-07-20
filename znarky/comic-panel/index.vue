38f722f395b657a354c930525a71bdda81712293
Emma-Jane MacKinnon-Lee <72898465+emmajane1313@users.noreply.github.com>
2022-07-19 22:49:52 -0400

    add next

--- znarky/comic-panel/index.vue
+++ znarky/comic-panel/index.vue
@@ -1,3 +1,4 @@
 SVG File Poignant Guide: "https://digitalax.mypinata.cloud/ipfs/QmdfZ7Z2dNfJMyDkCHSQQPpLVc7DiEXPiW7MEkU5fNGVzn",
 SVG File Poignant Guide Extended: "https://digitalax.mypinata.cloud/ipfs/QmPrKbCvZpvNPBZqDaXnXGhXakQYA5ZyWHJ2gjKiJFFxHF",
 SVG File Empty Closet: "https://f3manifesto.mypinata.cloud/ipfs/QmQ6RyvtniAXjJcXGfAarufP4sBb3YEPPzPMbs7UpkQFkS"
+SVG File CC0 Wave: "https://f3manifesto.mypinata.cloud/ipfs/QmS9zSsrLzM8dTZFRDjzhGPJ1J6y9TVVVrMvyPDqYMSg36"
