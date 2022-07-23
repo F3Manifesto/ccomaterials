55835162aedee1a656196472d7ea1240c9bb7ff7
Emma-Jane MacKinnon-Lee <72898465+emmajane1313@users.noreply.github.com>
2022-07-23 03:31:09 -0400

    add

--- howtomakeitinweb3/index.vue
+++ howtomakeitinweb3/index.vue
@@ -3,3 +3,8 @@
 "TV Movie File MP4": https://f3manifesto.mypinata.cloud/ipfs/QmYrVyzcSdD3XgRAXG1dBLG279SX7y2aGk3ViTP4Dj5Pgh,
 "TV Movie After Effects File": https://f3manifesto.mypinata.cloud/ipfs/QmWbmVN4N4ymGAu436htbWYxp7LaMySCdQNRnVSg7WUW6x,
 "TV Compoited Scene After Effects File w/ Music": https://f3manifesto.mypinata.cloud/ipfs/QmW3gAw8Tm8T2mdNeWnF3UUDJL881wr1dyrkg1ZJxLbDzV,
+
+
+// Diner Scene
+"Blender Scene File": https://f3manifesto.mypinata.cloud/ipfs/Qme1NFiYbAzDgd7aKFXPCrV8TsypxpY94AnT58kkDSbvsn,
+"Final Scene w/ Music": https://f3manifesto.mypinata.cloud/ipfs/QmRmxP7xA6MSgaW5oGp5GJGEiCju9TC9fVP1S9pczsWWZ4
