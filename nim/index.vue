6da10c9377e9074cf43f9998f31fe41e933e29ba
Emma-Jane MacKinnon-Lee <72898465+emmajane1313@users.noreply.github.com>
2022-07-24 17:12:37 -0400

    add

--- nim/index.vue
+++ nim/index.vue
@@ -3,11 +3,21 @@ Blender File: "https://digitalax.mypinata.cloud/ipfs/Qmd98RwJKnTyRZAv6FiBajfVYH8
 Material Image Files Zip: "https://f3manifesto.mypinata.cloud/ipfs/QmVpgXwAokzoyjMgJutR3jw8Hrax7StgXJJuZeQUYQgJz4",
 Render File: "https://f3manifesto.mypinata.cloud/ipfs/Qme9zic4g3kSRFteHuuHLPAoacaoKVqBdtKTgVmLLZdmSG",
 
-
 // Nim's Pose
 Blender File: "https://f3manifesto.mypinata.cloud/ipfs/QmcWDjSbU9k8KAPVLYQ4mwvi8LTa2vs55ESnj9FuTqHigg",
 Blender Video File: "https://f3manifesto.mypinata.cloud/ipfs/QmaHAK5KxDbjGwK9qzr9QmAzMDigGyPsGZWaNtAqLjK5rK",
 Render File: "https://f3manifesto.mypinata.cloud/ipfs/Qma2JEGrfivFAWPj9L2y6PsahXc4aTT3ogg6H4VR1M6GLu"
 
-//Nim's Accessory Pack:
+// Nim's Accessory Pack:
 Sketch File: "https://f3manifesto.mypinata.cloud/ipfs/Qmbic4api86JCT1qCNzaJc7ATBpnnFPPAKB3AKqxdF86gg"
+
+// Pocket Watch:
+Blender File: "https://f3manifesto.mypinata.cloud/ipfs/Qmbic4api86JCT1qCNzaJc7ATBpnnFPPAKB3AKqxdF86gg",
+Video File: "https://f3manifesto.mypinata.cloud/ipfs/QmReSJUVvjiVioXkChc6SWGCHZvCtyQvF2i6RyYL1BAMCc",
+Nim w/ PW Blender File: "https://f3manifesto.mypinata.cloud/ipfs/QmNc2ygGo5WV7sMk9Pm8KCb24A8iUEa9TaY3znZbw54X3T"
+
+// Sunglasses:
+Blender File: "https://f3manifesto.mypinata.cloud/ipfs/QmaBpoUrL767bfKvF5cYFoXTYLmnWUJQP8jWhyxnPwB3Gz",
+Video File: "https://f3manifesto.mypinata.cloud/ipfs/QmQNbNXiowmRGshXydU6HzAcEdYqAiFbrhrtqhzbYUiUE5",
+Nim w/ Sunglasses Blender File: "https://f3manifesto.mypinata.cloud/ipfs/QmNc2ygGo5WV7sMk9Pm8KCb24A8iUEa9TaY3znZbw54X3T"
+
